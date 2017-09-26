6e5afc75042be587047e25d122607d3d1b0f30a6
benjaminsunderland <ben@sunderland.eu.com>
2017-09-26 13:17:02 +0100

    Spec and src

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -5,8 +5,8 @@ describe('Javabuzz', function() {
   describe('knows when a number is', function() {
 
     it('divisible by 3', function () {
-
-
+      javabuzz = new Javabuzz();
+      expect(isDivisbleByThree(3)).toBe(true);
     });
 
   });
---
+++ src/fizzBuzz.js
@@ -0,0 +1,10 @@
+function Javabuzz() {
+
+};
+
+Javabuzz.prototype.isDivisbleByThree = function(number) {
+   return true
+};
+
+var javabuzz = new Javabuzz();
+console.log(javabuzz.isDivisbleByThree(3));
